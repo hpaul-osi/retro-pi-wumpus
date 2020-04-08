d54f66a95665f0aec9d1612444fc252cbeeb6725
Sachin-Srivastava <51713848+Sachin-Srivastava@users.noreply.github.com>
2020-04-08 16:40:24 -0400

    Add InsertValue API

--- source/backend/Function1.cs
+++ source/backend/Function1.cs
@@ -7,23 +7,17 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Timers;
+using OSIsoft.Identity;
+using OSIsoft.Data;
+using OSIsoft.Data.Http;
 
-namespace FunctionApp1
+namespace VotePolling
 {
     public static class Function1
     {
-        static List<string> users = new List<string>();
-        static Timer timer = new Timer();
-        static Object lockOfList = new Object();
-        static Object lockOfTimer = new Object();
-        static double votingInterval = 5000; // Voting resets every 5 seconds
-        static int VoteRoundNumber = 0;
-        
-        [FunctionName("RegisterUser")]
-        public static async Task<IActionResult> RegisterUser(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+        [FunctionName("VotePolling")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -31,84 +25,18 @@ namespace FunctionApp1
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var data = JsonConvert.DeserializeObject<WumpusActionVote>(requestBody);
+            // name = name ?? data?.name;
 
-            if (name != null)
-            {
-                lock (lockOfList)
-                {
-                    users.Add(name);
-                }
-                return (ActionResult)new OkResult();
-            }
-            else
-            {
-                return new BadRequestObjectResult("Please pass a name on the query string or in the request body");
-            }
-        }
+            var sDSWumpusData = new SDSWumpusData();
+            var voteData = (WumpusActionVote)data;
+            await sDSWumpusData.InsertWumpusValue(voteData);
 
-        [FunctionName("ListUsers")]
-        public static async Task<IActionResult> ListUsers(
-           [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
-           ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a ListUsers request.");
-            return (ActionResult)new OkObjectResult(users);
-        }
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-        [FunctionName("StartGame")]
-        public static async Task<IActionResult> StartGame(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a StartGame request.");
-
-            if (!timer.Enabled)
-            {
-                lock (lockOfTimer)
-                {
-                    if (!timer.Enabled)
-                    {
-                        timer.Enabled = true;
-                        timer.AutoReset = true;
-                        timer.Interval = votingInterval;
-                        timer.Elapsed += OnTimerElapsed;
-                        timer.Start();
-                    }
-                }
-            }
-
-            return (ActionResult)new OkResult();
-        }
-
-
-        [FunctionName("TryGetResult")]
-        public static async Task<IActionResult> TryGetResult(
-           [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
-           ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a TryGetResult request.");
-
-            string roundNumber = req.Query["VoteRoundNumber"];
-
-            if (string.IsNullOrEmpty(roundNumber))
-                return new BadRequestObjectResult("Please pass a VoteRoundNumber on the query string.");
-
-            if (int.Parse(roundNumber) <= VoteRoundNumber)
-            {
-                //ToDo: return (ActionResult)new OkObjectResult(AggregratedResult(roundNumber));
-                return (ActionResult)new OkResult();
-            }
-            else
-            {
-                return (ActionResult)new NoContentResult(); // Use NoContent to indicate not-ready
-            }
-        }
-
-        static void OnTimerElapsed(Object source, ElapsedEventArgs e) 
-        {
-            VoteRoundNumber++;
+            return new OkObjectResult(true);
         }
     }
 }
