2c51d594864cc930dfd1e278706b740115c1b7fd
drubinstein-osisoft <63362092+drubinstein-osisoft@users.noreply.github.com>
2020-04-09 10:26:06 -0400

    Update PIWumpus.py

--- source/frontend/PIWumpus.py
+++ source/frontend/PIWumpus.py
@@ -4,6 +4,7 @@ import kbhit
 import os
 import sys
 import time
+import ctypes
 import WumpusGameEngine
 
 # Constants
@@ -22,6 +23,33 @@ PORT = '443'
 TOKEN = '' # DON'T CHECK ME IN
 
 # Utils
+def set_font(font_name):
+    LF_FACESIZE = 32
+    STD_OUTPUT_HANDLE = -11
+
+    class COORD(ctypes.Structure):
+        _fields_ = [("X", ctypes.c_short), ("Y", ctypes.c_short)]
+
+    class CONSOLE_FONT_INFOEX(ctypes.Structure):
+        _fields_ = [("cbSize", ctypes.c_ulong),
+                    ("nFont", ctypes.c_ulong),
+                    ("dwFontSize", COORD),
+                    ("FontFamily", ctypes.c_uint),
+                    ("FontWeight", ctypes.c_uint),
+                    ("FaceName", ctypes.c_wchar * LF_FACESIZE)]
+
+    font = CONSOLE_FONT_INFOEX()
+    font.cbSize = ctypes.sizeof(CONSOLE_FONT_INFOEX)
+    font.nFont = 12
+    font.dwFontSize.X = 11
+    font.dwFontSize.Y = 18
+    font.FontFamily = 54
+    font.FontWeight = 400
+    font.FaceName = font_name
+
+    handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+    ctypes.windll.kernel32.SetCurrentConsoleFontEx(handle, ctypes.c_long(False), ctypes.pointer(font))
+
 def enable_ansi():
     if sys.platform[0 : 3] == "win":
         os.system('')
@@ -188,6 +216,8 @@ async def tryGetVoteResult(session, baseurl):
 async def main():
     global BASEURL
     global login
+
+    set_font("Impact")
 
     enable_ansi()
     WumpusGameEngine.init()
