c30775d6cb9464b32cb4d18b1e785e501fcb66c8
Harry Paul <hpaul@osisoft.com>
2020-04-08 22:29:10 -0400

    Added validation and set up http requests.

--- source/frontend/PIWumpus.py
+++ source/frontend/PIWumpus.py
@@ -1,9 +1,12 @@
+import aiohttp
+import asyncio
 import kbhit
 import os
 import sys
 import time
 import WumpusGameEngine
 
+# Constants
 ESC = chr(27)
 ANSI = ESC + "["
 CR = chr(13)
@@ -12,12 +15,10 @@ CHAT_START = 8
 CHAT_END = 22
 INPUT_LINE = 24
 
-REFRESH_INTERVAL = 1
+REFRESH_INTERVAL = 0.5
 
 HOST = 'localhost'
 PORT = '8080'
-URL = ""
-SESSION = ""
 
 # Utils
 def enable_ansi():
@@ -102,16 +103,15 @@ def get_cmd():
             done = True
             break
         cmd += input
-
-        idle()
     return cmd
 
-def idle():
-    add_chat("TODO: Print other users commands")
+def idle(session):
     # TODO poll server for chats
+    add_chat("TODO: Print other users commands")
     # TODO poll server for round results
+    tryGetVoteResult(session, URL)
 
-def game_screen():
+async def game_screen(session):
     clear_screen()
     print("Hold down CTRL and press C (and ignore the call stack) to quit ...")
     print()
@@ -119,22 +119,31 @@ def game_screen():
     WumpusGameEngine.displayRoomInfo()
     while True:
         cmd = get_cmd()
+        idle(session)
         print()
-        convert_cmd_to_data(cmd)
+        await convert_cmd_to_request(cmd, session)
         print("TODO: Send '{}' to server".format(cmd))
 
-def convert_cmd_to_data(command):
+def isInteger(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+async def convert_cmd_to_request(command, session):
     # JSH Assumption: It is better to have parsed the command into constituent parts for the backend
     # Move (M), Shoot (S), or Quit (Q)
     # TODO: We need to validate input sent to server.
     split_command = command.split()
     if len(split_command) > 1:
-        if (split_command[0] == "MOVE" or split_command[0] == "M"):
-            print("Sending Server Vote for MOVE")
-            postMoveVote(SESSION, URL, {"MOVE" : split_command[1]})
-        elif (split_command[0] == "SHOOT" or split_command[0] == "S"):
-            print("Sending Server Vote for SHOOT")
-            postMoveVote(SESSION, URL, {"SHOOT" : split_command[1]})
+        if (isInteger(split_command[1])):
+            if (split_command[0] == "MOVE" or split_command[0] == "M"):
+                print("Sending Server Vote for MOVE")
+                await postMoveVote(session, URL, {"MOVE" : split_command[1]})
+            elif (split_command[0] == "SHOOT" or split_command[0] == "S"):
+                print("Sending Server Vote for SHOOT")
+                await postMoveVote(session, URL, {"SHOOT" : split_command[1]})
     else:
         if (command == "QUIT" or command == "Q"):
             # TODO: Client gracefully exits the game
@@ -142,10 +151,6 @@ def convert_cmd_to_data(command):
         if (command == "HELP" or command == "H"):
             print("Requesting help...")
             WumpusGameEngine.show_instructions()
-
-async def init_connection_to_server(host, port):
-    async with aiohttp.ClientSession() as SESSION:
-        URL = 'http://{}:{}'.format(host, port)
 
 async def postMoveVote(session, url, data):
     # InsertValue API call
@@ -162,20 +167,22 @@ async def postRegisterUser(session, url, data):
     async with session.post(url, json=data) as response:
         return await response.text()
 
-async def GetListUsers(session, url, data):
+async def getListUsers(session, url):
     # ListUsers API call
-    async with session.post(url, json=data) as response:
+    async with session.get(url) as response:
         return await response.text()
 
-async def GetVoteResult(session, url, data):
-    # ListUsers API call
-    async with session.post(url, json=data) as response:
+# TODO: need to add round number...
+async def tryGetVoteResult(session, url):
+    # TryGetAgreggate API call
+    async with session.get(url) as response:
         return await response.text()
 
-def main():
+async def main():
+    global URL
+    
     enable_ansi()
     WumpusGameEngine.init()
-    init_connection_to_server(HOST, PORT)
 
     clear_screen()
     print("Welcome to PI Wumpus!")
@@ -188,8 +195,10 @@ def main():
     clear_screen()
     WumpusGameEngine.banner()
 
-    game_screen()
+    async with aiohttp.ClientSession() as session:
+        URL = 'http://{}:{}'.format(HOST, PORT)
+        await game_screen(session)
 
 if __name__ == "__main__":
-    main()
-
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
